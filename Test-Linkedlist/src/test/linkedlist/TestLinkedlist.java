cb144b7372df2e336f5696981117c48b41d65ce9
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-07-20 17:38:02 +0500

    Add files via upload

---
+++ Test-Linkedlist/src/test/linkedlist/TestLinkedlist.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package test.linkedlist;
+
+/**
+ *
+ * @author Ahsan Khalid
+ */
+public class TestLinkedlist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
