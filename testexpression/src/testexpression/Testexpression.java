4495df0333d9d1a9766010ac7a8121c96a3ad810
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-07-30 01:49:10 +0500

    Add files via upload

---
+++ testexpression/src/testexpression/Testexpression.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package testexpression;
+
+/**
+ *
+ * @author Ahsan Khalid
+ */
+public class Testexpression {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int res=5+9-10/20*14;
+        System.out.println(" res :"+res);
+    }
+
+}
