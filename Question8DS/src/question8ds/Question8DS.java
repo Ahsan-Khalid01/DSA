8563743bb69b3ba2f85eccb6d7401cc13bc4addc
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-08-02 01:20:44 +0500

    Add files via upload

---
+++ Question8DS/src/question8ds/Question8DS.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package question8ds;
+
+/**
+ *
+ * @author Ahsan Khalid
+ */
+public class Question8DS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
