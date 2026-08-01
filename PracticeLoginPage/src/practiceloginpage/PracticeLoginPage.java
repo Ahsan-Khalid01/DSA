6ecf3048d5f3b6d68d25bd828e7c7bdc1674d386
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-08-02 01:21:55 +0500

    Add files via upload

---
+++ PracticeLoginPage/src/practiceloginpage/PracticeLoginPage.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package practiceloginpage;
+
+/**
+ *
+ * @author Ahsan Khalid
+ */
+public class PracticeLoginPage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Wclass o = new Wclass();
+        o.setSize(400,600);
+        o.setLayout(null);
+        o.setTitle("Login Page");
+        o.setDefaultCloseOperation(3);
+        o.setVisible(true);
+        o.setLocationRelativeTo(null);
+    }
+
+}
---
+++ PracticeLoginPage/src/practiceloginpage/Wclass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package practiceloginpage;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+
+/**
+ *
+ * @author Ahsan Khalid
+ */
+public class Wclass extends JFrame{
+
+}
